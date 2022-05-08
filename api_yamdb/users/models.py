70d37daf429fecdb5bea5c61e5d04080b047d943
Alexander Vorona <sumchatyj@Alexanders-MacBook-Air.local>
2022-05-09 00:34:25 +0300

    User model extenshion

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+
+
+class User(AbstractUser):
+    is_moderator = models.BooleanField(
+        _('moderator status'),
+        default=False,
+        help_text=_("Designates whether the user can use moderator's "
+                    "permissions.")
+    )
