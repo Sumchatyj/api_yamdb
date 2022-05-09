14af87dd29ae4ae2b0f3694c639a15c6aa89eac5
Татьяна Мамаева <tanjaka06@gmail.com>
2022-05-09 14:41:48 +0300

    add models

---
+++ api_yamdb/api/models.py
@@ -0,0 +1,89 @@
+from datetime import date
+
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+from users.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Gerne(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+# возможно стоит переопределить year
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name="titles",
+        null=True
+    )
+    gerne = models.ForeignKey(
+        Gerne,
+        on_delete=models.SET_NULL,
+        related_name="titles",
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    score = models.PositiveIntegerField(
+        default=5,
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return self.text[:15]
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return self.text[:15]
